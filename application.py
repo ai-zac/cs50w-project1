241f96507dba76aa5a41c19114a1e6102924800b
usan31 <usan31showtek@gmail.com>
2022-10-04 22:07:17 -0600

    Usando el bootstrap compilado y agregado del sistema import

--- application.py
+++ application.py
@@ -1,12 +1,11 @@
 import os
+import secrets
 import requests
 
-from flask import Flask, session, render_template, flash, request, redirect
-from flask_session import Session
+from flask import Flask, session, render_template, flash, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import check_password_hash, generate_password_hash
-
 
 app = Flask(__name__)
 
@@ -14,13 +13,13 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
+# Configure session to ufrom required import login_requiredse filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.secret_key = secrets.token_hex()
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"))  
 db = scoped_session(sessionmaker(bind=engine))
 
 
@@ -32,14 +31,14 @@ def login():
         password = request.form.get("password")
 
         # BASE DE DATOS INTERACTIVE
-        user = db.execute("""SELECT id, username, hash FROM users WHERE username = ?""", username)
+        user = db.execute("SELECT id, nombre, contrasena FROM usuarios WHERE nombre = :username", {"username": username}).fetchone()
 
         if not user:
             flash("the user doesnt exist")
             return render_template("login.html")
 
-        if check_password_hash(user[0]["hash"], password):
-            session["user_id"] = user[0]["id"]
+        if check_password_hash(user[2], password):
+            session["user_id"] = user[0]
             return redirect("/")
         else:
             flash("username or password isnt correct")
@@ -67,9 +66,8 @@ def register():
 
         hash = generate_password_hash(password)
 
-        # BASE DE DATOS INTERACTIVE
-        db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", 
-                   {"username": username, "hash": hash})
+        db.execute("INSERT INTO usuarios(nombre, contrasena) VALUES(:nombre, :contrasena)", 
+                   {"nombre": username, "contrasena": hash})
         db.commit()
 
         flash("The register was sucesfully")
@@ -88,5 +86,3 @@ def index():
 
 
 
-if __name__ == "__main__":
-    app.run()
--- import.py
+++ import.py
@@ -1,27 +1,24 @@
-<<<<<<< HEAD
-=======
-import os 
+import os
 import csv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
 
 def main():
-    books = open("vuelos.csv")
-    rows = csv.reader(books)
+    with open("books.csv") as books:
+        rows = csv.DictReader(books)
+        for row in rows:
+            db.execute("INSERT INTO libros(isbn, titulo, autor, año_publicacion) VALUES (:isbn, :title, :author, :year)",
+                    {"isbn": row["isbn"], "title": row["title"], "author": row["author"], "year": row["year"]})
+            db.commit()
 
-    db.execute()
-    db.commit()
-
-if __name__ == "__main__":
-    main()
->>>>>>> 13397466578be6b40c37fff4276a75dc50ad7767
+if (__name__ == "__main__"):
+    main() 
